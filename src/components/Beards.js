5d053e2a85d584a6d69d54c9700edd384e181cfe
Wes Cutting <wesleycutting@gmail.com>
2018-02-05 13:24:02 -0700

    Merge Example

--- src/components/Beards.js
+++ src/components/Beards.js
@@ -8,6 +8,7 @@ class Beards extends Component {
         <h3>Beardly Fellow</h3>
         <p>He likes to grow out his beard and stuff</p>
         <p>Oh man! We are learning things and stuff</p>
+        <p>New Line from Teacher Machine</p>
       </div>
     );
   }
