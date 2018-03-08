719b3496e8d8d4c470146d8cedfa94f89d2ae1bf
Wes Cutting <wesleycutting@gmail.com>
2018-03-08 10:09:41 -0700

    Example of GitKraken in Action

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import '../style/Beards.css';
 
-class Beards extends Component {
+
+export default class extends Component {
   render() {
     return (
       <div className="Beards">
@@ -9,10 +9,8 @@ class Beards extends Component {
         <p>He likes to grow out his beard and stuff</p>
         <p>New Line from Student Machine</p>
         <p>Oh man! We are learning things and stuff</p>
-        <p>I really like beards yo!</p>
       </div>
     );
   }
 }
 
-export default Beards;
